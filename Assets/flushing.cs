8dd1496c90bfcafe72df19d5b7406cd01e4c40db
Jonas Graf <grafj@hs-weingarten.de>
2016-01-24 12:21:51 +0100

    flush that shit down !!
    ERROR not getting out of toilet anymore

---
+++ Assets/flushing.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class flushing : MonoBehaviour {
+    private Canvas canvas;
+    private GameObject toiletText;
+    private GameObject textToilet;
+    private bool flush = false;
+    private AudioSource audio;
+	// Use this for initialization
+	void Start () {
+        canvas = GameObject.Find ("Canvas").GetComponent<Canvas> ();
+
+        toiletText = Resources.Load ("Prefabs/toiletText") as GameObject;
+        textToilet = Instantiate (toiletText, new Vector2(Screen.width/2,20), Quaternion.identity) as GameObject;
+        textToilet.transform.parent = canvas.transform;
+        textToilet.SetActive (false);
+        audio = gameObject.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (flush)
+        {
+            if (Input.GetKeyDown("f"))
+            {
+                audio.Play();
+            }
+        }
+	    
+	}
+
+    void OnTriggerEnter(Collider col){
+        if (col.tag == "Player")
+        {
+            flush = true;
+            textToilet.SetActive (true);
+        }
+    }
+    void OnTriggerExit(Collider col){
+        if (col.tag == "Player")
+        {
+            flush = false;
+            textToilet.SetActive (false);
+            audio.Stop();
+        }
+    }
+}
